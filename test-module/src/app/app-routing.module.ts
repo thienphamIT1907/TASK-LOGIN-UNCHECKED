0ab556c5ba94e4377b3cef62b54e536ca6627834
Thien Pham <phamanhthien1907@gmail.com>
2020-09-07 21:52:35 +0700

    Update demo routing from other module

---
+++ test-module/src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'action',
+    loadChildren: () => import('./login/login.module').then(m => m.LoginModule)
+  }
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule { }
